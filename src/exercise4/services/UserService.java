30c2cc879b850d3af77f3363554e3d9e9713d725
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-07 11:20:36 -0300

    (exercise4) Adds create method to user service.

--- src/exercise4/services/UserService.java
+++ src/exercise4/services/UserService.java
@@ -1,5 +1,6 @@
 package exercise4.services;
 
+import exercise4.factories.UserFactory;
 import exercise4.models.User;
 import exercise4.strategies.ValidationStrategy;
 
@@ -8,5 +9,14 @@ public class UserService {
 
   public UserService(ValidationStrategy validator) {
     this.validator = validator;
+  }
+
+  public User create(String name, String email) {
+    if (this.validator.isValid(email)) {
+      System.out.println("Email address is invalid!");
+      return null;
+    }
+
+    return UserFactory.create(name, email);
   }
 }

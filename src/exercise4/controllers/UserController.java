896658094689403d02524bc589c882f27ceaa684
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-08 12:14:01 -0300

    (exercise4) Refact createUser() method in user controller.

--- src/exercise4/controllers/UserController.java
+++ src/exercise4/controllers/UserController.java
@@ -1,12 +1,13 @@
 package exercise4.controllers;
 
+import exercise4.models.User;
 import exercise4.services.UserService;
 import exercise4.strategies.EmailValidation;
 
 public class UserController {
-  private static UserService userService = new UserService(new EmailValidation());
+  private static final UserService userService = new UserService(new EmailValidation());
 
-  public static void createUser(String name, String email) {
-    userService.create(name, email);
+  public static User createUser(String name, String email) {
+    return userService.create(name, email);
   }
 }

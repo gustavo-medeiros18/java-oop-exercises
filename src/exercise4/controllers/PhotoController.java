7655f9080d1d8c839746c74005c9011e55c8f5a8
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-11 14:39:56 -0300

    (exercise4) Adds definition of PhotoController code.

--- src/exercise4/controllers/PhotoController.java
+++ src/exercise4/controllers/PhotoController.java
@@ -1,4 +1,17 @@
 package exercise4.controllers;
 
+import exercise4.factories.PhotoFactory;
+import exercise4.models.Photo;
+import exercise4.services.PhotoService;
+import exercise4.strategies.UrlValidation;
+
+import java.util.Date;
+
 public class PhotoController {
+  private static final PhotoService photoService =
+      new PhotoService(new UrlValidation());
+
+  public static Photo createPhoto(String url, Date uploadDate, int userId) {
+    return photoService.create(url, uploadDate, userId);
+  }
 }

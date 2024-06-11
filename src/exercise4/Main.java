43a84132e7a76e627297d814dd52767fbf58367f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-11 16:08:40 -0300

    (exercise4) Updates Main.java basic test code.

--- src/exercise4/Main.java
+++ src/exercise4/Main.java
@@ -1,35 +1,23 @@
 package exercise4;
 
-import exercise4.controllers.PhotoController;
+
 import exercise4.controllers.UserController;
-import exercise4.models.Photo;
+
 import exercise4.models.User;
 
-import java.util.Date;
-import java.util.Scanner;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    // Criando três usuários
+    UserController.create("User1", "user1@email.com");
+    UserController.create("User2", "user2@email.com");
+    UserController.create("User3", "user3@email.com");
 
-    System.out.print("Enter user's name: ");
-    String name = input.nextLine();
-
-    System.out.print("Enter user's email: ");
-    String email = input.next();
-
-    User user = UserController.createUser(name, email);
-    System.out.println(user);
-
-    System.out.println("-------------------");
-
-    System.out.print("Enter photo's user ID: ");
-    int id = input.nextInt();
-
-    System.out.print("Enter photo's URL: ");
-    String url = input.next();
-
-    Photo photo = PhotoController.createPhoto(url, new Date(), id);
-    System.out.println(photo);
+    // Exibindo os usuários criados
+    List<User> users = UserController.index();
+    for (User user : users) {
+      System.out.println("ID: " + user.getId() + ", Name: " + user.getName() + ", Email: " + user.getEmail());
+    }
   }
 }

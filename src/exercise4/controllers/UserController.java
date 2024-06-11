a83fb623fc203526abb9128637a7050ce52220b8
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-11 16:07:43 -0300

    (exercise4) Adds UserController.index() method.

--- src/exercise4/controllers/UserController.java
+++ src/exercise4/controllers/UserController.java
@@ -4,11 +4,17 @@ import exercise4.models.User;
 import exercise4.services.UserService;
 import exercise4.strategies.EmailValidation;
 
+import java.util.List;
+
 public class UserController {
   private static final UserService userService =
       new UserService(new EmailValidation());
 
   public static User create(String name, String email) {
     return userService.create(name, email);
+  }
+
+  public static List<User> index() {
+    return userService.index();
   }
 }

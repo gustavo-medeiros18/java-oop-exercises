1081f168a4e1225a790e0a4241bc3758660eee70
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-12 09:58:43 -0300

    (exercise4) Adds index() method to photo controller.

--- src/exercise4/controllers/PhotoController.java
+++ src/exercise4/controllers/PhotoController.java
@@ -1,17 +1,21 @@
 package exercise4.controllers;
 
-import exercise4.factories.PhotoFactory;
 import exercise4.models.Photo;
 import exercise4.services.PhotoService;
 import exercise4.strategies.UrlValidation;
 
 import java.util.Date;
+import java.util.List;
 
 public class PhotoController {
   private static final PhotoService photoService =
       new PhotoService(new UrlValidation());
 
-  public static Photo createPhoto(String url, Date uploadDate, int userId) {
+  public static Photo create(String url, Date uploadDate, int userId) {
     return photoService.create(url, uploadDate, userId);
+  }
+
+  public static List<Photo> index() {
+    return photoService.index();
   }
 }

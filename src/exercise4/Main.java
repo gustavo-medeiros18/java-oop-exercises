a84f42947ca5f517696a321b9062f047266fba3a
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-12 10:17:58 -0300

    (exercise4) Refact Main.java basic tests.

--- src/exercise4/Main.java
+++ src/exercise4/Main.java
@@ -1,6 +1,5 @@
 package exercise4;
 
-
 import exercise4.controllers.PhotoController;
 import exercise4.controllers.UserController;
 
@@ -12,24 +11,40 @@ import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    // Criando três usuários
+    testUserCreation();
+    testPhotoCreation();
+    testUserDisplay();
+  }
+
+  private static void testUserCreation() {
+    System.out.println("Testing User Creation:");
     UserController.create("User1", "user1@email.com");
     UserController.create("User2", "user2@email.com");
     UserController.create("User3", "user3@email.com");
 
-    // Exibindo os usuários criados
     List<User> users = UserController.index();
     for (User user : users) {
       System.out.println(user);
     }
+    System.out.println("User Creation Test Completed.\n");
+  }
 
+  private static void testPhotoCreation() {
+    System.out.println("Testing Photo Creation:");
     PhotoController.create("https://photo1.com", new Date(), 1);
     PhotoController.create("https://photo2.com", new Date(), 2);
 
     List<Photo> photos = PhotoController.index();
-
     for (Photo photo : photos) {
       System.out.println(photo);
     }
+    System.out.println("Photo Creation Test Completed.\n");
+  }
+
+  private static void testUserDisplay() {
+    System.out.println("Testing User Display:");
+    User user = UserController.show(1);
+    System.out.println(user);
+    System.out.println("User Display Test Completed.\n");
   }
 }

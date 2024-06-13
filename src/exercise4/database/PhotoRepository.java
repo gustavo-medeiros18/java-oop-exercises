f8d818f9bcd5b84f28207f9e44f2d30e76dd6abf
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-13 11:36:59 -0300

    (exercise4) Adds update method to photo repository.

--- src/exercise4/database/PhotoRepository.java
+++ src/exercise4/database/PhotoRepository.java
@@ -31,6 +31,34 @@ public class PhotoRepository {
     return photos;
   }
 
+  public static void update(int id, Photo newPhotoData) {
+    Photo photo = find(id);
+
+    if (photo != null) {
+      // Find the user who owns the photo
+      User user = UserRepository.find(photo.getUserId());
+
+      // Update the photo details
+      photo.setUploadDate(newPhotoData.getUploadDate());
+      photo.setUrl(newPhotoData.getUrl());
+      photo.setUserId(newPhotoData.getUserId());
+
+      // Update the photo in the main photos list
+      photos.set(id - 1, photo);
+
+      // If the user exists and has this photo, update it in the user's photo list
+      if (user != null) {
+        List<Photo> userPhotos = user.getPhotos();
+        for (int i = 0; i < userPhotos.size(); i++) {
+          if (userPhotos.get(i).getId() == id) {
+            userPhotos.set(i, photo);
+            break;
+          }
+        }
+      }
+    }
+  }
+
   public static void delete(int id) {
     Photo photo = find(id);
 

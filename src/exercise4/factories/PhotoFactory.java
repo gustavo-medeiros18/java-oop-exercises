01ea94ee2fd6e662175c7b0ba37e2cdae7b8cd6f
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-14 14:13:54 -0300

    (exercise4) Makes photo id don't be set in factory.

--- src/exercise4/factories/PhotoFactory.java
+++ src/exercise4/factories/PhotoFactory.java
@@ -7,6 +7,6 @@ public class PhotoFactory {
   private static int idCount = 0;
 
   public static Photo create(String url, Date uploadDate, int userId) {
-    return new Photo(++idCount, url, uploadDate, userId);
+    return new Photo(url, uploadDate, userId);
   }
 }

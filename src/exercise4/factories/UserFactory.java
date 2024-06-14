0db84e254c9b9d8aed8bc3f56864819f6d3656e0
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-14 15:08:34 -0300

    (exercise4) Removes id increment from user factory.

--- src/exercise4/factories/UserFactory.java
+++ src/exercise4/factories/UserFactory.java
@@ -3,13 +3,7 @@ package exercise4.factories;
 import exercise4.models.User;
 
 public class UserFactory {
-  private static int idCount = 0;
-
   public static User create(String name, String email) {
-    return new User(++idCount, name, email);
-  }
-
-  public static User create(int id, String name, String email) {
-    return new User(id, name, email);
+    return new User(name, email);
   }
 }

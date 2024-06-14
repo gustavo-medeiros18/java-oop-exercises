653ef677b803b89599181b02fd3895bef1061b84
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-14 12:01:32 -0300

    (exercise4) Adds exception if url is invalid and user id doesn't exists in create and update methods.

--- src/exercise4/services/PhotoService.java
+++ src/exercise4/services/PhotoService.java
@@ -1,7 +1,11 @@
 package exercise4.services;
 
 import exercise4.database.PhotoRepository;
+import exercise4.database.UserRepository;
+import exercise4.exceptions.InvalidUrlException;
+import exercise4.exceptions.UserNotFoundException;
 import exercise4.models.Photo;
+import exercise4.models.User;
 import exercise4.strategies.ValidationStrategy;
 
 import java.util.List;
@@ -14,13 +18,13 @@ public class PhotoService {
   }
 
   public Photo create(Photo photo) {
-    if (!this.validator.isValid(photo.getUrl())) {
-      System.out.println("URL is invalid!");
-      return null;
-    }
+    if (!this.validator.isValid(photo.getUrl()))
+      throw new InvalidUrlException("URL is invalid!");
+
+    User user = UserRepository.find(photo.getUserId());
+    if (user == null) throw new UserNotFoundException("User not found!");
 
     PhotoRepository.add(photo);
-
     return photo;
   }
 
@@ -33,6 +37,12 @@ public class PhotoService {
   }
 
   public void update(int id, Photo newPhotoData) {
+    if (!this.validator.isValid(newPhotoData.getUrl()))
+      throw new InvalidUrlException("URL is invalid!");
+
+    User user = UserRepository.find(newPhotoData.getUserId());
+    if (user == null) throw new UserNotFoundException("User not found!");
+
     PhotoRepository.update(id, newPhotoData);
   }
 

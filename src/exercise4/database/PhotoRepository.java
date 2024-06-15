2de6ded04f7e669bee2b061084c946d154d00b4b
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-15 12:00:55 -0300

    (exercise4) Updates PhotoRepository.update() method to move the photo to another user list in case of change in userId.

--- src/exercise4/database/PhotoRepository.java
+++ src/exercise4/database/PhotoRepository.java
@@ -41,27 +41,25 @@ public class PhotoRepository {
     if (photo == null)
       throw new PhotoNotFoundException("Photo with id " + id + " not found.");
 
-    User user = UserRepository.find(photo.getUserId());
-
-    if (user == null)
+    User oldUser = UserRepository.find(photo.getUserId());
+    if (oldUser == null)
       throw new UserNotFoundException("User with id " + photo.getUserId() + " not found.");
 
-    // Update the photo details
     photo.setUploadDate(newPhotoData.getUploadDate());
     photo.setUrl(newPhotoData.getUrl());
-    photo.setUserId(newPhotoData.getUserId());
 
-    // Update the photo in the main photos list
-    photos.set(id - 1, photo);
+    if (photo.getUserId() != newPhotoData.getUserId()) {
+      oldUser.getPhotos().remove(photo);
 
-    // If the user exists and has this photo, update it in the user's photo list
-    List<Photo> userPhotos = user.getPhotos();
-    for (int i = 0; i < userPhotos.size(); i++) {
-      if (userPhotos.get(i).getId() == id) {
-        userPhotos.set(i, photo);
-        break;
-      }
+      User newUser = UserRepository.find(newPhotoData.getUserId());
+      if (newUser == null)
+        throw new UserNotFoundException("User with id " + newPhotoData.getUserId() + " not found.");
+
+      newUser.getPhotos().add(photo);
+      photo.setUserId(newPhotoData.getUserId());
     }
+
+    photos.set(id - 1, photo);
   }
 
   public static void delete(int id) {

23a17891cd81aa30490a5ade956c06a4eade33de
Gustavo Medeiros <dev.gusmedeiros@gmail.com>
2024-06-26 13:45:22 -0300

    (exercise8) Adds updateSeller() method.

--- src/exercise8/application/Program.java
+++ src/exercise8/application/Program.java
@@ -10,7 +10,9 @@ public class Program {
   public static void main(String[] args) throws SQLException {
 //    retrieveAllDepartments();
 //    insertSeller();
-    insertDepartments();
+//    insertDepartments();
+    updateSeller();
+
   }
 
   private static void retrieveAllDepartments() {
@@ -101,6 +103,32 @@ public class Program {
         DB.closeResultSet(rs);
       } else
         System.out.println("No rows affected!");
+    } catch (SQLException e) {
+      System.out.println(e.getMessage());
+    } finally {
+      DB.closeStatement(st);
+      DB.closeConnection();
+    }
+  }
+
+  private static void updateSeller() {
+    Connection conn = null;
+    PreparedStatement st = null;
+
+    try {
+      conn = DB.getConnection();
+      st = conn.prepareStatement(
+          "UPDATE seller "
+              + "SET BaseSalary = BaseSalary + ? "
+              + "WHERE "
+              + "(DepartmentId = ?)"
+      );
+
+      st.setDouble(1, 200.0);
+      st.setInt(2, 2);
+
+      int rowsAffected = st.executeUpdate();
+      System.out.println("Done! Rows affected: " + rowsAffected);
     } catch (SQLException e) {
       System.out.println(e.getMessage());
     } finally {
